2ebf78fd387da748700be3f5ac5f915629281bfc
Amirhossein Salmani <amirhosseinsalmanidev@gmail.com>
2025-02-24 17:42:16 -0500

    fix

--- components/ThreeScene.tsx
+++ components/ThreeScene.tsx
@@ -2,12 +2,8 @@
 
 import { useRef, useEffect } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
-import { PerspectiveCamera, Effects } from "@react-three/drei"
+import { PerspectiveCamera } from "@react-three/drei"
 import * as THREE from "three"
-import { UnrealBloomPass } from "three-stdlib"
-import { extend } from "@react-three/fiber"
-
-extend({ UnrealBloomPass })
 
 function Cube() {
   const meshRef = useRef<THREE.Mesh>(null)
@@ -17,8 +13,8 @@ function Cube() {
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      const x = (event.clientY / window.innerHeight - 0.5) * Math.PI * 0.5
-      const y = (event.clientX / window.innerWidth - 0.5) * Math.PI * 0.5
+      const x = (event.clientY / window.innerHeight - 0.5) * Math.PI * 0.3
+      const y = (event.clientX / window.innerWidth - 0.5) * Math.PI * 0.3
       targetRotation.current = { x, y }
     }
 
@@ -43,36 +39,23 @@ function Cube() {
   })
 
   return (
-    <group scale={[3, 3, 3]}>
-      {" "}
-      {/* Increased size */}
+    <group scale={[2.5, 2.5, 2.5]}>
       <mesh ref={meshRef}>
         <boxGeometry args={[2, 2, 2]} />
         <meshBasicMaterial color="#050301" transparent opacity={0.1} />
       </mesh>
+
       <lineSegments ref={edgesRef}>
         <edgesGeometry args={[new THREE.BoxGeometry(2, 2, 2)]} />
-        <lineBasicMaterial color="#00dc82" linewidth={2} /> {/* Increased line width */}
+        <lineBasicMaterial color="#00dc82" linewidth={1.5} />
       </lineSegments>
     </group>
-  )
-}
-
-function Scene() {
-  return (
-    <>
-      <Cube />
-      <Effects>
-        <unrealBloomPass threshold={0.1} strength={1.5} radius={1} />
-      </Effects>
-    </>
   )
 }
 
 export default function ThreeScene({ onLoad }: { onLoad?: () => void }) {
   useEffect(() => {
     if (onLoad) {
-      // Delay the onLoad callback slightly to ensure smooth animation
       const timer = setTimeout(onLoad, 500)
       return () => clearTimeout(timer)
     }
@@ -81,11 +64,10 @@ export default function ThreeScene({ onLoad }: { onLoad?: () => void }) {
   return (
     <div className="absolute inset-0 -z-10">
       <Canvas>
-        <PerspectiveCamera makeDefault position={[0, 0, 12]} /> {/* Adjusted camera distance */}
+        <PerspectiveCamera makeDefault position={[0, 0, 10]} />
         <color attach="background" args={["#050301"]} />
-        <ambientLight intensity={0.1} />
         <pointLight position={[10, 10, 10]} intensity={0.5} color="#00dc82" />
-        <Scene />
+        <Cube />
       </Canvas>
     </div>
   )

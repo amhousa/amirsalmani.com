a55d5824c985dc821e576256c777e205207fdd6c
Amirhossein Salmani <amirhosseinsalmanidev@gmail.com>
2025-02-26 18:56:04 -0500

    add icon png and favicon

--- scripts/generate-favicons.ts
+++ scripts/generate-favicons.ts
@@ -1,5 +1,5 @@
-import sharp from "sharp"
-import path from "path"
+import * as sharp from "sharp";
+import * as path from "path";
 
 async function generateFavicons() {
   const sizes = {
@@ -9,21 +9,20 @@ async function generateFavicons() {
     "apple-touch-icon.png": 180,
     "android-chrome-192x192.png": 192,
     "android-chrome-512x512.png": 512,
-  }
+  };
 
-  const sourceIcon = path.join(process.cwd(), "public", "icon.svg")
+  const sourceIcon = path.join(process.cwd(), "public", "icon.svg");
 
   for (const [filename, size] of Object.entries(sizes)) {
     await sharp(sourceIcon)
       .resize(size, size)
-      .toFile(path.join(process.cwd(), "public", filename))
+      .toFile(path.join(process.cwd(), "public", filename));
   }
 
   // Generate ICO file (contains multiple sizes)
   await sharp(sourceIcon)
     .resize(32, 32)
-    .toFile(path.join(process.cwd(), "public", "favicon.ico"))
+    .toFile(path.join(process.cwd(), "public", "favicon.ico"));
 }
 
-generateFavicons().catch(console.error)
-
+generateFavicons().catch(console.error);

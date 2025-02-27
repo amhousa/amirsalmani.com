1262c90fde492402fcfc99b1d495a572265d78eb
Amirhossein Salmani <amirhosseinsalmanidev@gmail.com>
2025-02-26 21:56:41 -0500

    add feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
@@ -27,6 +28,7 @@ export default function Home() {
         </Link>
       </div>
       <Analytics />
+      <SpeedInsights/>
     </div>
   )
 }
